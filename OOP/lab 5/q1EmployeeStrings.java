d589b8763ae758dce130c8166d35f59c4eadaf73
Arnav Jagia <arnavjagia04@gmail.com>
2023-10-02 23:18:33 +0530

    name formatting fix

--- OOP/lab 5/q1EmployeeStrings.java
+++ OOP/lab 5/q1EmployeeStrings.java
@@ -24,19 +24,30 @@ class Employee {
     void formatEmployeeName()
     {
         String nName="";
+        Ename = Ename.strip();
         int len = Ename.length();
-
+        /*
+        "  JOHN  DOE  "
+        */
         char firstChar = Ename.charAt(0);
-        nName += (!isWhitespace(firstChar) && !isCaps(firstChar)) ? firstChar : firstChar - 32;
-        for(int i = 1; i < len; i++)
+        nName += (isCaps(firstChar)) ? (char) (firstChar) : (char) (firstChar - 32);
+        for(int i = 1; i < len-1; i++)
         {
-            char ele = Ename.charAt(i);
-            nName += (!isWhitespace(ele) && isCaps(ele)) ? ele : ele - 32;
+            char present = Ename.charAt(i), next = Ename.charAt(i+1);
+            if (isSpace(present)) {
+                if (isSpace(next)) continue;
+                nName += (isCaps(next)) ? (char) (next) : (char) (next - 32); i++;
+            }
+            else {
+                nName += (isCaps(present)) ? (char) (present + 32) : (char) (present);
+            }
         }
+        char endChar = Ename.charAt(len-1);
+        nName += (isCaps(endChar)) ? (char) (endChar + 32) : (char) (endChar);
         Ename = nName;
     }
-    private boolean isCaps(char c) {return c>=65 && c<=90;}
-    private boolean isWhitespace(char c) {return c == 32;}
+    private boolean isCaps(char c) { return c>=65 && c<=90; }
+    private boolean isSpace(char c) { return c == 32; }
 
     String generateEmail()
     {
@@ -53,7 +64,8 @@ class Employee {
 
 public class q1EmployeeStrings {
     public static void main(String[] args) {
-        Employee emp = new Employee("JOHN DOE", 12, 100.01);
+        // Employee emp = new Employee("JOHN DOE", 12, 100.01);
+        Employee emp = new Employee("   JOHN    DOE   ", 12, 100.01);
         emp.display();
         emp.formatEmployeeName();
         emp.display();
